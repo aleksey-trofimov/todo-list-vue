79ee4b43afd76ee155db909fc025fdc1cf504afa
aleksey-trofimov <context.pskov@yandex.ru>
2022-03-28 17:17:14 +0300

    Создание шаблона HTML с роутингом Vue - продолжение

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<!-- Отображает только заголовок -->
+<template>
+  <div id="header">
+    <router-link id="home" to="/">Todo List Vue</router-link>
+  </div>
+</template>
+
+<style lang="css" scoped>
+#header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin-top: 0;
+  padding: 30px;
+  background: #2c3e50;
+  color: white;
+  display: flex;
+  justify-content: center;
+  border-bottom: 5px solid #4e6c71;
+}
+
+#header a {
+  font-size: 40px;
+  text-decoration-color: red;
+  display: inline-block;
+  color: white;
+  opacity: 0.7;
+}
+
+#header a:hover {
+  opacity: 1;
+}
+</style>
---
+++ src/components/TheMenu.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="menu">
+    <router-link id="" to="/edit">Новая запись</router-link>
+    <router-link id="" to="/tech">Тех. задание</router-link>
+    <router-link id="" to="/about">О приложении</router-link>
+  </div>
+</template>
+
+<style lang="css" scoped>
+#menu {
+  position: fixed;
+  top: 112px;
+  left: 0;
+  right: 0;
+  margin-top: 0;
+  padding: 7px;
+  background: #3f5369;
+  color: white;
+  display: flex;
+  justify-content: flex-start;
+  border-bottom: 5px solid #8f9fa1;
+}
+
+#menu a {
+  font-size: 23px;
+  padding-left: 5%;
+  display: inline-block;
+  color: white;
+  opacity: 0.7;
+}
+
+#menu a:hover {
+  opacity: 1;
+}
+
+#menu a.active-link-class {
+  text-decoration-color: red;
+}
+</style>
